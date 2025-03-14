702759449bb28d7bd7957dc5974fc2f5e8c3274d
lmorisse <laurent.morisseau@gmail.com>
2025-03-14 18:31:23 +0100

    intro => index

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
             items: [
               {
                 label: 'Guide',
-                to: '/docs/intro',
+                to: '/docs/',
               },
             ],
           },
