8e0cec631ffa78cf1519e9a781d7c8202880d24f
lmorisse <laurent.morisseau@gmail.com>
2025-04-19 18:12:00 +0200

    Update index.js

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--primary button--lg"
+            className="button button--secondary button--lg"
             to="/docs/">
             Guide Agile4Enterprise
           </Link>
